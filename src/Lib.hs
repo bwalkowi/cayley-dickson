84d1a84439494ff0bbc18f6d4f4594adf12c2c3e
Wojciech Karpiel <wkarpiel@student.agh.edu.pl>
2017-03-13 13:00:02 +0100

    init v2

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,107 +3,76 @@ module Lib
     ) where
 
 someFunc :: IO ()
-someFunc = putStrLn ("someFunc " ++ (show d) ++ "\n  " ++(show eqq))
-
-d :: CDnum Double
-d = divCD (Pair (Base 0.0) (Base 1.0))  (Base 320.0)
-
-eqqq = 2
-eqq:: {-- --}CDnum Double
-eqq = {-- getFirstBase $ --} sqNormCD (Pair
-                 (Pair (Base 10.0) (Base 5.0))
-                 ( Base 13.0 ))
-{--
-Konstrukcja Cayleya-Dicksona
-
-Konstrukcja Cayleya-Dicksona jest metodą rozszerzania unormowanej przestrzeni liniowej przez tworzenie par jej elementów ( a , b ) {\displaystyle (a,\;b)} {\displaystyle (a,\;b)}, a następnie definiowanie działań w następujący sposób:
---}
-
-data CDnum a = Pair (CDnum a) (CDnum a) | Base a deriving (Eq, Show)
+someFunc = putStrLn "Elo"
 
 
-fillMissing :: (Operations a) => (CDnum a-> CDnum a -> CDnum a) -> CDnum a-> CDnum a -> CDnum a
-fillMissing fn (Pair a b) (Base c) = fillMissing fn (Pair a b) (Pair (Base c) (neutralAddCD (Base  c)))
-fillMissing fn (Base a) (Pair c d) = fillMissing fn (Pair (Base a) (neutralAddCD (Base a))) (Pair c d)
-fillMissing fn a b = fn a b
-
-simplify :: (Operations a) => CDnum a -> CDnum a
-simplify (Base a) = Base a
-simplify (Pair a b) = if (neutralAddCD b) == b  then simplify a  else (Pair a b)
-
-getFirstBase :: (Operations a) => CDnum a-> a
-getFirstBase (Base a) =a
-getFirstBase (Pair a _) = getFirstBase a
-
-addCD :: (Operations a) =>  CDnum a -> CDnum a -> CDnum a 
-addCD (Base a) (Base b) = Base (add a b)
-addCD (Pair a1 b1) (Pair a2 b2) = Pair (addCD a1 a2) (addCD b1 b2)
-addCD a b =  fillMissing addCD a b
-
---figuring out neutrall addition element requires getting an example specimen
-neutralAddCD :: (Operations a) => CDnum a ->  CDnum a
-neutralAddCD (Base _) = (Base neutralAdd)
-neutralAddCD num = (Pair (neutralAddCD num) (neutralAddCD num))
-
-oppositeAddCD :: (Operations a) => CDnum a -> CDnum a
-oppositeAddCD (Base a) = Base (oppositeAdd a)
-oppositeAddCD (Pair a b) = Pair (oppositeAddCD a) (oppositeAddCD b)
-
-subCD :: (Operations a) => CDnum a -> CDnum a -> CDnum a
-subCD a b = addCD a (oppositeAddCD b)
-
-mulCD :: (Operations a) => CDnum a -> CDnum a -> CDnum a
-mulCD (Base a) (Base b) = Base (mul a b)
-mulCD (Pair a1 b1) (Pair a2 b2) = Pair
-  (subCD (mulCD a1 a2) (mulCD (conjugateCD b2) b1))
-  (addCD (mulCD a1 b2) (mulCD (conjugateCD a2) b1))
-mulCD a b  = fillMissing mulCD a b
-
---requires example specimen
-neutralMulCD :: (Operations a) => CDnum a -> CDnum a
-neutralMulCD (Base _) = Base neutralMul
-neutralMulCD (Pair a b) = Pair (neutralMulCD a) (neutralAddCD b) --todo to A i B nie powinno być potrzebne. do modelowania zagnieżdżeń przydałby się IDric albo co
-
-oppositeMulCD :: (Operations a) => CDnum a -> CDnum a
-oppositeMulCD (Base a) = Base (oppositeMul a)
-oppositeMulCD (Pair a b) = Pair
-  (divCD (conjugateCD a) (addCD (sqNormCD a) (sqNormCD b)))
-  (oppositeAddCD (divCD b (addCD (sqNormCD a) (sqNormCD b))))
---todo sqrNorm to powinno 'a' zwracać!!
---ale z wikipedii: a* `mul`  a = |a|^2 
-sqNormCD :: (Operations a) => CDnum a -> CDnum a
-sqNormCD a = (mulCD (conjugateCD a) a)
-
-
-divCD :: (Operations a) => CDnum a -> CDnum a -> CDnum a
-divCD a b = mulCD a (oppositeMulCD b)
-
-conjugateCD :: (Operations a) => CDnum a -> CDnum a --sprzężenia
-conjugateCD (Base a) = Base (conjugate a)
-conjugateCD (Pair a b) = Pair (conjugateCD a) (oppositeAddCD b)
-
-
---TODO: 'a' tworzy unormowaną przestrzeń liniową
-class  (Eq a) => Operations a where
-  add :: a -> a -> a
+class (Fractional a) => CDOps a where
   neutralAdd :: a
-  oppositeAdd :: a -> a
-  mul :: a -> a -> a
   neutralMul :: a
-  oppositeMul :: a -> a
-  conjugate :: a-> a --sprzężony
-  sqr :: a -> a
-  --norma :: a -> a
+  conjugate :: a -> a
+  normSqr :: a -> a
+  normSqr a = a * (conjugate a)
 
-instance Operations Double where
-  add = (+)
+instance CDOps Double where
   neutralAdd = 0
-  oppositeAdd a = -a
-  mul = (*)
   neutralMul = 1
-  oppositeMul a = 1/a
   conjugate a = a
-  sqr = sqrt 
-  ---norma a = a
-    
-  
+
+data CDNum a = Base a | Pair (CDNum a)  (CDNum a) deriving (Show, Eq)
+
+simplify :: (Eq a, CDOps a) => CDNum a -> CDNum a
+simplify (Base a) = Base a
+simplify (Pair a b) = if b == cdNeutralAdd
+  then simplify a
+  else (Pair a b)
+
+cdExtend :: (CDOps a) => CDNum a -> CDNum a
+cdExtend a = Pair a (Base neutralAdd)
+
+callWithEqualNestedness :: (CDOps a) => (CDNum a-> CDNum a-> CDNum a) -> CDNum a-> CDNum a-> CDNum a
+callWithEqualNestedness fn (Base a1) (Pair a2 b2) =
+  callWithEqualNestedness fn (cdExtend (Base a1)) (Pair a2 b2)
+callWithEqualNestedness fn (Pair a1 b1) (Base a2) =
+  callWithEqualNestedness fn (Pair a1 b1) (cdExtend (Base a2))
+callWithEqualNestedness fn a b = fn a b
+
+cdAdd :: (CDOps a) => CDNum a -> CDNum a -> CDNum a
+cdAdd (Base a) (Base b) = Base (a+b)
+cdAdd (Pair a1 b1) (Pair a2 b2) = Pair (cdAdd a1 a2) (cdAdd b1 b2)
+cdAdd a b = callWithEqualNestedness cdAdd a b
+
+cdNeutralAdd :: (CDOps a) => CDNum a
+cdNeutralAdd =  Pair (Base neutralAdd) (Base neutralAdd)
+
+cdNegative :: (CDOps a) => CDNum a-> CDNum a
+cdNegative (Base a) = Base (-a)
+cdNegative (Pair a b) = Pair (cdNegative a) (cdNegative b)
+
+cdSubstract ::  (CDOps a) => CDNum a -> CDNum a -> CDNum a
+cdSubstract a b = cdAdd a (cdNegative b)
+
+cdMultiply :: (CDOps a) => CDNum a -> CDNum a -> CDNum a
+cdMultiply (Base a) (Base b) = Base (a*b)
+cdMultiply (Pair a1 b1) (Pair a2 b2) = Pair
+  (cdSubstract (cdMultiply a1 a2) (cdMultiply (cdConjugate b2) b1))
+  (cdAdd (cdMultiply a1 b2) (cdMultiply (cdConjugate a2) b1))
+cdMultiply a b = callWithEqualNestedness cdMultiply a b
+
+cdNeutralMultiply :: (CDOps a) => CDNum a
+cdNeutralMultiply = Pair (Base neutralMul) (Base neutralAdd)
+
+cdRecip ::(CDOps a) => CDNum a-> CDNum a
+cdRecip (Base a) = Base (recip a)
+cdRecip (Pair a b) = Pair
+  (cdDivide (cdConjugate a) (cdAdd (cdNormSqr a) (cdNormSqr b)))
+  (cdNegative (cdDivide b (cdAdd (cdNormSqr a) (cdNormSqr b))))
+
+cdDivide :: (CDOps a) => CDNum a-> CDNum a -> CDNum a
+cdDivide a b = cdMultiply a (cdRecip b)
+
+cdConjugate :: (CDOps a) => CDNum a -> CDNum a
+cdConjugate (Base a) = Base (conjugate a)
+cdConjugate (Pair a b) = Pair (cdConjugate a) (cdNegative b)
+
+cdNormSqr :: (CDOps a) =>CDNum a -> CDNum a
+cdNormSqr  a = cdMultiply (cdConjugate a) a
