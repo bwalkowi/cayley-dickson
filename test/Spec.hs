8557f13424ca4e748e4253040b6004024e9f4c15
Wojciech Karpiel <wkarpiel@student.agh.edu.pl>
2017-03-27 11:51:00 +0200

    tesy stub

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,11 +1,16 @@
 
+{-# LANGUAGE ScopedTypeVariables, TemplateHaskell #-}
 import Test.QuickCheck
-import Test.HUnit
-import Test.Framework
-import Test.Framework.Providers.HUnit
+--import Test.HUnit
+--import Test.Framework
+--import Test.Framework.Providers.HUnit
 import Data.Monoid
 import Control.Monad
 --import Utils
- 
-main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+
+
+
+prop_revapp :: [Int] -> [Int] -> Bool
+prop_revapp xs ys = reverse (xs++ys) == reverse xs ++ reverse ys
+
+main = quickCheck prop_revapp
