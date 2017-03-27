1f097ef18ca6928c0c99ff0f823b86b3cc57af3c
Wojciech Karpiel <wkarpiel@student.agh.edu.pl>
2017-03-27 12:53:08 +0200

    smutne testy

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,16 +1,27 @@
-
 {-# LANGUAGE ScopedTypeVariables, TemplateHaskell #-}
 import Test.QuickCheck
+import CDParser
+import Lib
 --import Test.HUnit
 --import Test.Framework
 --import Test.Framework.Providers.HUnit
 import Data.Monoid
 import Control.Monad
---import Utils
 
 
+instance (Arbitrary a, Eq a, Fractional a) => Arbitrary (CDNum a) where
+  arbitrary = do
+    t <- arbitrary
+    ts <- arbitrary
+    return (Pair t ts)
 
 prop_revapp :: [Int] -> [Int] -> Bool
-prop_revapp xs ys = reverse (xs++ys) == reverse xs ++ reverse ys
+prop_revapp xs ys = reverse (xs++ys) ==reverse ys++ reverse xs
 
-main = quickCheck prop_revapp
+prop_simplify ::  (CDNum Double) -> Bool
+prop_simplify num =(simplify (Pair num neutralAdd)) == (simplify num)
+
+main = do
+  quickCheck prop_revapp
+  quickCheck prop_simplify 
+
