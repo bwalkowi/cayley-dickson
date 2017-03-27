d1507b3c4358772afda19764223a1f242adedff5
Wojciech Karpiel <wkarpiel@student.agh.edu.pl>
2017-03-27 10:51:25 +0200

    + zalążek testów

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,11 @@
+
+import Test.QuickCheck
+import Test.HUnit
+import Test.Framework
+import Test.Framework.Providers.HUnit
+import Data.Monoid
+import Control.Monad
+--import Utils
+ 
 main :: IO ()
 main = putStrLn "Test suite not yet implemented"
